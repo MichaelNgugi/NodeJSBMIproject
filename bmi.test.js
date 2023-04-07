aa188c13c0e9f0255f0f36f80426fd2ac1737bf2
Michael Ngugi <michaelrealz17@gmail.com>
2023-04-07 23:26:14 +0300

    UI testing

--- bmi.test.js
+++ bmi.test.js
@@ -1,4 +1,5 @@
 const {calculateBMI, bmiStatus} = require('./calculations');
+const puppeteer = require("expect-puppeteer");
 
 test ('50Kg, 1m bmi=50', () => {
     expect(calculateBMI(50, 1)).toBe(50);
@@ -6,4 +7,26 @@ test ('50Kg, 1m bmi=50', () => {
 
 test ('bmi=50 status=normal', () => {
     expect(bmiStatus(50)).toBe("Normal weight");
+});
+
+describe ('testing with puppeteer' , () =>{
+    beforeAll(async () => {
+        //const browser = await puppeteer.launch({ headless: true });
+        //const page = await browser.newPage();
+        await page.goto("/", { waitUntil: 'load' });
+    });
+
+    it('should display analysis page', async () => {
+        const analysisBtn = await page.$('#all_data');
+        await analysisBtn.click();
+            
+        await page.waitForTimeout(2000);
+
+        const pageTitle = await page.title();
+
+        //assert(pageTitle == 'BMI Reports');
+        expect(pageTitle).toEqual("BMI Reports");
+
+        await browser.close();
+    });
 });
